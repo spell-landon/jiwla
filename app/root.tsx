3f90c35d4f91044466f5b7d4e520f17c27ed5f03
spell-landon <spell.landon@gmail.com>
2024-01-30 22:37:01 -0600

    Reinitialize

--- app/root.tsx
+++ app/root.tsx
@@ -51,36 +51,6 @@ export default function App() {
           href='https://fonts.googleapis.com/css2?family=Sono:wght@300&family=Special+Elite&family=Stardos+Stencil:wght@400;700&family=Teko:wght@300&display=swap'
           rel='stylesheet'></link>
         <Links />
-        {/* <!-- update the version number as needed --> */}
-        <script defer src='/__/firebase/10.7.2/firebase-app-compat.js'></script>
-        {/* <!-- include only the Firebase features as you need --> */}
-        <script
-          defer
-          src='/__/firebase/10.7.2/firebase-auth-compat.js'></script>
-        <script
-          defer
-          src='/__/firebase/10.7.2/firebase-database-compat.js'></script>
-        <script
-          defer
-          src='/__/firebase/10.7.2/firebase-firestore-compat.js'></script>
-        <script
-          defer
-          src='/__/firebase/10.7.2/firebase-functions-compat.js'></script>
-        <script
-          defer
-          src='/__/firebase/10.7.2/firebase-messaging-compat.js'></script>
-        <script
-          defer
-          src='/__/firebase/10.7.2/firebase-storage-compat.js'></script>
-        <script
-          defer
-          src='/__/firebase/10.7.2/firebase-analytics-compat.js'></script>
-        <script
-          defer
-          src='/__/firebase/10.7.2/firebase-remote-config-compat.js'></script>
-        <script
-          defer
-          src='/__/firebase/10.7.2/firebase-performance-compat.js'></script>
       </head>
       <body className='font-SpecialElite bg-tertiary/50'>
         <Layout>
