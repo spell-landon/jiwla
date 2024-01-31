601f434575016abc56ae73589bb849d396bda9d7
spell-landon <spell.landon@gmail.com>
2024-01-30 22:33:06 -0600

    Add scripts and change destination to root

--- app/root.tsx
+++ app/root.tsx
@@ -32,12 +32,14 @@ export default function App() {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <Meta />
+        <meta name='msapplication-TileColor' content='#874142' />
+        <meta name='theme-color' content='#ffffff'></meta>
+
         <link rel='apple-touch-icon' sizes='180x180' href={apple_touch_icon} />
         <link rel='icon' type='image/png' sizes='32x32' href={favicon_32_32} />
         <link rel='icon' type='image/png' sizes='16x16' href={favicon_16_16} />
         <link rel='manifest' href={site_manifest} />
-        <meta name='msapplication-TileColor' content='#874142' />
-        <meta name='theme-color' content='#ffffff'></meta>
 
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
@@ -48,8 +50,37 @@ export default function App() {
         <link
           href='https://fonts.googleapis.com/css2?family=Sono:wght@300&family=Special+Elite&family=Stardos+Stencil:wght@400;700&family=Teko:wght@300&display=swap'
           rel='stylesheet'></link>
-        <Meta />
         <Links />
+        {/* <!-- update the version number as needed --> */}
+        <script defer src='/__/firebase/10.7.2/firebase-app-compat.js'></script>
+        {/* <!-- include only the Firebase features as you need --> */}
+        <script
+          defer
+          src='/__/firebase/10.7.2/firebase-auth-compat.js'></script>
+        <script
+          defer
+          src='/__/firebase/10.7.2/firebase-database-compat.js'></script>
+        <script
+          defer
+          src='/__/firebase/10.7.2/firebase-firestore-compat.js'></script>
+        <script
+          defer
+          src='/__/firebase/10.7.2/firebase-functions-compat.js'></script>
+        <script
+          defer
+          src='/__/firebase/10.7.2/firebase-messaging-compat.js'></script>
+        <script
+          defer
+          src='/__/firebase/10.7.2/firebase-storage-compat.js'></script>
+        <script
+          defer
+          src='/__/firebase/10.7.2/firebase-analytics-compat.js'></script>
+        <script
+          defer
+          src='/__/firebase/10.7.2/firebase-remote-config-compat.js'></script>
+        <script
+          defer
+          src='/__/firebase/10.7.2/firebase-performance-compat.js'></script>
       </head>
       <body className='font-SpecialElite bg-tertiary/50'>
         <Layout>
